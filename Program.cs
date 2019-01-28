dec4015698bfa7a4417236ee48afe7712bf5de03
stuartd <stuartd@gmail.com>
2019-01-28 16:13:41 +0000

    Tweaks

--- Program.cs
+++ Program.cs
@@ -3,10 +3,12 @@
 namespace Working_Days_Calculator {
     class Program {
         static void Main(string[] args) {
-			int month = 12;
-			int year = 2018;
+			int month = 1;
+			int year = 2019;
 			bool writeDays = true;
-			const decimal dailyRate = 344.83M;
+			bool writeDaysIncludingBankHolidays = false;
+            const decimal dailyRate = 344.83M;
+			const decimal adjustment = -0.5M;
 			
 			var start = new DateTime(year, month, 1);
 			var end = new DateTime(year, month, DateTime.DaysInMonth(year, month));
@@ -14,11 +16,19 @@ namespace Working_Days_Calculator {
 			Console.WriteLine($"Working days in {month}/{year}");
 			var days = WorkingDaysCalculator.GetWorkingDays(start, end);
 
-			Console.WriteLine($"{days.Length} days @ £{dailyRate} per day");
+			Console.WriteLine($"Actual days: {days.Length}");
+
+            var dayCount = days.Length + adjustment;
+
+			if (adjustment != 0M) {
+				Console.WriteLine($"Adjusted days: {dayCount}");
+            }
+
+			Console.WriteLine($"{dayCount} days @ £{dailyRate} per day");
 
 			Console.WriteLine();
 
-            Console.WriteLine($"{dailyRate * days.Length:C2}");
+            Console.WriteLine($"{dailyRate * dayCount:C2}");
 
 			Console.WriteLine();
 
@@ -26,7 +36,9 @@ namespace Working_Days_Calculator {
 				foreach (var day in days) {
 					Console.WriteLine(day.Date.ToShortDateString());
 				}
+			}
 
+			if (writeDaysIncludingBankHolidays) { 
 				Console.WriteLine("Working days (including bank holidays");
 				days = WorkingDaysCalculator.GetWorkingDaysExcludingBankHolidays(year, month);
 				Console.WriteLine($"{days.Length} days");
